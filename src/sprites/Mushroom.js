7a1b9cd256104762aea7fbc7d4d3a42855393133
Luke Berry <contact@luke-berry.co.uk>
2018-11-10 00:54:12 +0000

    Sprite rotation + velocity

--- src/sprites/Mushroom.js
+++ src/sprites/Mushroom.js
@@ -3,17 +3,35 @@ import Phaser from 'phaser'
 class Mushroom extends Phaser.Sprite {
   constructor ({ game, x, y, asset }) {
     super(game, x, y, asset)
-    this.anchor.setTo(0.5)
+
+    this.anchor.setTo(0.48)
     this.game = game
-    this.cursors = this.game.input.keyboard.createCursorKeys()
+    this.cursors = game.input.keyboard.createCursorKeys()
+    this.boomBoom = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+
+    this.game.physics.enable(this)
+    this.body.drag = { x: 1500, y: 1500 }
+    this.body.angularDrag = 150
+    this.body.maxAngular = 500
+    this.body.collideWorldBounds = true
+    this.body.bounce.setTo(1, 1)
   }
 
   update () {
-    if (this.cursors.left.isDown) {
-      this.body.velocity.x -= 10
+    if (this.cursors.up.isDown) {
+      this.game.physics.arcade.accelerationFromRotation(this.rotation, 50, this.body.acceleration)
+    } else {
+      this.body.acceleration.set(0)
     }
-    if (this.cursors.right.isDown) {
-      this.body.velocity.x += 10
+
+    if (this.cursors.left.isDown) {
+      this.body.angularVelocity += -10
+    } else if (this.cursors.right.isDown) {
+      this.body.angularVelocity += 10
+    }
+
+    if (this.boomBoom.isDown) {
+      this.game.physics.arcade.accelerationFromRotation(this.rotation, 500, this.body.acceleration)
     }
   }
 }
